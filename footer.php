bb8ce6e56977022f03d09d7649890fce741d67ea
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-07 14:22:53 +0100

    modif footer

--- footer.php
+++ footer.php
@@ -20,7 +20,7 @@
             <div class="row">
                 <!--Grid column-->
                 <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Footer Content</h5>
+                <h5 class="text-uppercase">eXim</h5>
 
                 <p>
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
@@ -32,20 +32,14 @@
 
                 <!--Grid column-->
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Links</h5>
+                <h5 class="text-uppercase">Plan du site</h5>
 
                 <ul class="list-unstyled mb-0">
                     <li>
-                    <a href="#!" class="text-white">Link 1</a>
+                    <a href="#!" class="text-white">Accueil</a>
                     </li>
                     <li>
-                    <a href="#!" class="text-white">Link 2</a>
-                    </li>
-                    <li>
-                    <a href="#!" class="text-white">Link 3</a>
-                    </li>
-                    <li>
-                    <a href="#!" class="text-white">Link 4</a>
+                    <a href="#!" class="text-white">Articles</a>
                     </li>
                 </ul>
                 </div>
@@ -53,20 +47,14 @@
 
                 <!--Grid column-->
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase mb-0">Links</h5>
+                <h5 class="text-uppercase mb-0">Contact</h5>
 
                 <ul class="list-unstyled">
                     <li>
-                    <a href="#!" class="text-white">Link 1</a>
+                    <a href="#!" class="text-white">E-mail</a>
                     </li>
                     <li>
-                    <a href="#!" class="text-white">Link 2</a>
-                    </li>
-                    <li>
-                    <a href="#!" class="text-white">Link 3</a>
-                    </li>
-                    <li>
-                    <a href="#!" class="text-white">Link 4</a>
+                    <a href="#!" class="text-white">Téléphone</a>
                     </li>
                 </ul>
                 </div>
@@ -78,8 +66,8 @@
 
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            © 2020 Copyright:
-            <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+            © 2023 Copyright:
+            <a class="text-white" href="https://exim.com/">eXim.com</a>
             </div>
             <!-- Copyright -->
         </footer>
