ad23142bd3ab139b0ef55e2761c9af231c5def82
TristanREB <tristan.rebeyrol@mmibordeaux.com>
2023-02-07 18:02:50 +0100

    Likes commit

--- views/backend/dashboard.php
+++ views/backend/dashboard.php
@@ -77,9 +77,9 @@ include '../../header.php';
                             <td>Likes</td>
                             <td>
                                 <a href="/views/backend/likes/list.php" class="btn btn-primary ">List</a>
-                                <a href="/views/backend/likes/create.php" class="btn btn-success disabled">Create</a>
-                                <a href="/views/backend/likes/edit.php" class="btn btn-warning disabled">Edit</a>
-                                <a href="/views/backend/likes/delete.php" class="btn btn-danger disabled">Delete</a>
+                                <a href="/views/backend/likes/create.php" class="btn btn-success ">Create</a>
+                                <!-- <a href="/views/backend/likes/edit.php" class="btn btn-warning disabled">Edit</a>
+                                <a href="/views/backend/likes/delete.php" class="btn btn-danger disabled">Delete</a> -->
                             </td>
                             <td>Utilisation de Ajax si trop de temps ;)</td>
                         </tr>
