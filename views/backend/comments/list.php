0c0de188c305765884981e918f64f122cb98c87b
Safirl <loic.leforestier@mmibordeaux.com>
2023-02-07 15:02:50 +0100

    Delete button works in comments

--- views/backend/comments/list.php
+++ views/backend/comments/list.php
@@ -36,7 +36,7 @@ $commentaires = sql_select("COMMENT", "*");
                             <td><?php echo $commentaire['numMemb']; ?></td>
                             <td>
                                 <a href="edit.php?id=<?php echo $commentaire['numCom']; ?>" class="btn btn-primary">Edit</a>
-                                <a href="delete.php?numStat=<?php echo $commentaire['numCom']; ?>" class="btn btn-danger">Delete</a>
+                                <a href="delete.php?numCom=<?php echo $commentaire['numCom']; ?>" class="btn btn-danger">Delete</a>
                             </td>
                         </tr>
                     <?php } ?>
