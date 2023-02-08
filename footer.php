bf8a2f75246b302e219788a3c82dfd884ae78500
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-08 14:46:24 +0100

    Ajout de la bordure footer et correction bug page connection (front)

--- footer.php
+++ footer.php
@@ -12,59 +12,62 @@
     <link rel="stylesheet" href="/src/css/footer.css">
 </head>
 <body>
-    <section class="">
-        <!-- Footer -->
-        <footer class="text-center">
-            <!-- Grid container -->
-            <div class="container p-4">
-            <!--Grid row-->
-            <div class="row text-start">
-                <!--Grid column-->
-                <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
-                    <img src="/images/eXim-logo.svg" alt="logo">
-                </div>
+    <div>
+        <div class="bordure-footer"></div>
+        <section class="">
+            <!-- Footer -->
+            <footer class="text-center">
+                <!-- Grid container -->
+                <div class="container p-4">
+                <!--Grid row-->
+                <div class="row text-start">
                     <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0"> 
-                    <h5 class="text-uppercase">Plan du site</h5>
+                    <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+                        <img src="/images/eXim-logo.svg" alt="logo">
+                    </div>
+                        <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0"> 
+                        <h5 class="text-uppercase">Plan du site</h5>
 
-                    <ul class="list-unstyled mb-0">
-                        <li>
-                        <a href="#!" class="liens">Accueil</a>
-                        </li>
-                        <li>
-                        <a href="#!" class="liens">Articles</a>
-                        </li>
-                    </ul>
+                        <ul class="list-unstyled mb-0">
+                            <li>
+                            <a href="#!" class="liens">Accueil</a>
+                            </li>
+                            <li>
+                            <a href="#!" class="liens">Articles</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-lg-3 col-md-6 mb-4 mb-md-0 last-in-footer">
+                        <h5 class="text-uppercase mb-0">Contact</h5>
+
+                        <ul class="list-unstyled">
+                            <li>
+                            <a href="#!" class="liens">E-mail : abcd@exim-contact.com</a>
+                            </li>
+                            <li>
+                            <a href="#!" class="liens">Téléphone : 01 10 20 30 40</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!--Grid column-->
                 </div>
-                <!--Grid column-->
-
-                <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0 last-in-footer">
-                    <h5 class="text-uppercase mb-0">Contact</h5>
-
-                    <ul class="list-unstyled">
-                        <li>
-                        <a href="#!" class="liens">E-mail : abcd@exim-contact.com</a>
-                        </li>
-                        <li>
-                        <a href="#!" class="liens">Téléphone : 01 10 20 30 40</a>
-                        </li>
-                    </ul>
+                <!--Grid row-->
                 </div>
-                <!--Grid column-->
-            </div>
-            <!--Grid row-->
-            </div>
-            <!-- Grid container -->
+                <!-- Grid container -->
 
-            <!-- Copyright -->
-            <div class="text-center p-3">
-            © eXim, Tous droits réservés . 
-            <a class="liens" href="https://exim.com/"> Mentions Légales</a>
-            </div>
-            <!-- Copyright -->
-        </footer>
-        <!-- Footer -->
+                <!-- Copyright -->
+                <div class="text-center p-3">
+                © eXim, Tous droits réservés . 
+                <a class="liens" href="https://exim.com/"> Mentions Légales</a>
+                </div>
+                <!-- Copyright -->
+            </footer>
+            <!-- Footer -->
         </section>
+    </div>
 </body>
 </html>
