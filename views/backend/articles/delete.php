be81419c67034a69bc183e9c4d2ac1dd12828237
Héloise <heloise.pingitore@mmibordeaux.com>
2023-02-08 11:53:25 +0100

    delete Article

--- views/backend/articles/delete.php
+++ views/backend/articles/delete.php
@@ -13,7 +13,7 @@ include '../../../header.php';
 
 $numArt = $_GET['numArt'];
 
-$article = sql_select("article", "*", "numArt = $numArt");
+$article = sql_select("article", "*", "numArt = $numArt")[0];
 
 
 ?>
@@ -22,7 +22,7 @@ $article = sql_select("article", "*", "numArt = $numArt");
 <div class="container">
     <div class="row">
         <div class="col-md-12">
-            <h1>Delete Status</h1>
+            <h1>Delete Article</h1>
         </div>
         <div class="col-md-12">
             <!--Form to create a new status-->
@@ -30,7 +30,18 @@ $article = sql_select("article", "*", "numArt = $numArt");
                 <div class="form-group">
                     <label for="article">article</label>
                     
-                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($numArt); ?>" readonly="readonly">
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($numArt); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['dtCreArt']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libTitrArt']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libChapoArt']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libAccrochArt']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['parag1Art']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libSsTitr1Art']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['parag2Art']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libSsTitr2Art']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['parag3Art']); ?>" readonly="readonly"><br>
+                    <input id="article" class="form-control" type="text" name="numArt" value="<?php echo($article['libConclArt']); ?>" readonly="readonly"><br>
+
                 </div>
                 <div class="form-group mt-2">
                     <button type="submit" class="btn btn-danger">Confirm Deletion ?</button>
