c871c218d1520d01368bc9f54cd31298967fcfaa
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-08 12:15:03 +0100

    PAGE CONNECTIONNN FINIII

--- views/backend/login.php
+++ views/backend/login.php
@@ -51,37 +51,44 @@ $passMemb = $_GET['passMemb'];
                     <input id="passMemb" class="form-control taille-saisie" type="password" name="passMemb">
 
                 </div>
-                <div class="password-icon">
-                    <i data-feather="eye"></i>
-                    <i data-feather="eye-off"></i>
+                <div class="text-mdp">
+                    <div class="password-icon">
+                        <i data-feather="eye"></i>
+                        <i data-feather="eye-off"></i>
+                    </div>
+                    <!-- ICON SCRIPT -->
+                    <script src="https://unpkg.com/feather-icons"></script>
+                    <script>
+                    feather.replace();
+
+                    const eye = document.querySelector(".feather-eye");
+                    const eyeoff = document.querySelector(".feather-eye-off");
+                    const passwordField = document.querySelector("input[type=password]");
+
+                    eye.addEventListener("click", () => {
+                    eye.style.display = "none";
+                    eyeoff.style.display = "block";
+
+                    passwordField.type = "text";
+                    });
+
+                    eyeoff.addEventListener("click", () => {
+                    eyeoff.style.display = "none";
+                    eye.style.display = "block";
+
+                    passwordField.type = "password";
+                    });
+                    </script>
+
+                    <p class="text-connect">Afficher / masquer le mot de passe</p>
                 </div>
-                <!-- ICON SCRIPT -->
-                <script src="https://unpkg.com/feather-icons"></script>
-                <script>
-                feather.replace();
-
-                const eye = document.querySelector(".feather-eye");
-                const eyeoff = document.querySelector(".feather-eye-off");
-                const passwordField = document.querySelector("input[type=password]");
-
-                eye.addEventListener("click", () => {
-                eye.style.display = "none";
-                eyeoff.style.display = "block";
-
-                passwordField.type = "text";
-                });
-
-                eyeoff.addEventListener("click", () => {
-                eyeoff.style.display = "none";
-                eye.style.display = "block";
-
-                passwordField.type = "password";
-                });
-                </script>
-
 
                 <div class="form-group mt-2">
                     <button type="submit" class="conect">Se connecter</button>
+                </div>
+                <div class="align-btn-connect">
+                    <p class="text-connect">Vous n’avez pas de compte ?</p>
+                    <a href="#!" class="liens-ariane text-connect">Créer un compte.</a>
                 </div>
             </form>
         </div>
