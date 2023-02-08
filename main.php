3736e185194f4a611228df62d4eb2b8fe8cc1fdb
EloLsn <elodie.laisne@mmibordeaux.com>
2023-02-08 14:02:59 +0100

    mainsuite

--- main.php
+++ main.php
@@ -59,14 +59,17 @@
 <h1>Quelques expériences immersives qui pourraient vous plaire</h1>
     <div class="rect-decouvrir">
         <div class="bloc-decouvrir">
+            <h3>évènement 1</h3>
             <div class="info-decouvrir">
             </div>
         </div>   
         <div class="bloc-decouvrir">
+        <h3>évènement 2</h3>
             <div class="info-decouvrir">
             </div>
         </div>
         <div class="bloc-decouvrir">
+        <h3>évènement 3</h3>
             <div class="info-decouvrir">
             </div>
         </div>
