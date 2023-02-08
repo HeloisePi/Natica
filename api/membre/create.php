c4552bd948f6040df1773be0d08db6b039dca083
TristanREB <tristan.rebeyrol@mmibordeaux.com>
2023-02-08 18:38:01 +0100

    Messages erreurs et renvoi vers page admin ou client selon la checkbox

--- api/membre/create.php
+++ api/membre/create.php
@@ -14,15 +14,15 @@ $pseudoMemb =  sql_escape($_POST['pseudoMemb']);
 if (sql_select('MEMBRE', 'eMailMemb', "eMailMemb = '$eMailMemb'")[0]['eMailMemb'] == $eMailMemb) {
     if (!check_access(1) || !check_access(2)) {
         header('Location: ../../../views/backend/inscription.php?error=1');
+        // print_r('1');
+    } else {
+        header('Location: ../../../views/backend/members/list.php?error=1');
     }
 } else {
-
     // var_dump($_POST);
     sql_insert(
         'membre',
         "prenomMemb, nomMemb, pseudoMemb, passMemb, eMailMemb, numStat",
         "'$prenomMemb','$nomMemb','$pseudoMemb','$passMemb','$eMailMemb', 3"
     );
-
-    header('Location: ../../../views/backend/members/list.php');
 }
