788ded4af28d1c3ca19bd9a634fd9accaf471ce3
Safirl <loic.leforestier@mmibordeaux.com>
2023-02-09 20:29:21 +0100

    intégration de lisez aussi. !! Manque le CSS

--- evenement.php
+++ evenement.php
@@ -141,18 +141,15 @@ $comments = sql_select('ARTICLE INNER JOIN COMMENT ON ARTICLE.numArt = COMMENT.n
             <img class="espace-img-col2" src="/assert/svg/Personnes-a-mobilite-reduite-RVB.svg" alt="Pictograme d'une poussette"><br>
             <p class="titre-col-2">Lisez aussi :</p>
             <div class="rect-all">
-            <?php $articles = sql_select('article', '*', "", "numArt DESC", 3);?>
-                <?php foreach ($articles as $article) { ?>
+            <?php $lisezAussis = sql_select('article', '*', "", "numArt DESC", 3);?>
+                <?php foreach ($lisezAussis as $lisezAussi) { ?>
                     <div class="titres-all">
-                        <?php echo ('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>'); ?>
+                        <?php echo ('<a href="/evenement.php?numArt=' . $lisezAussi['numArt'] . ' "><h3>' . $lisezAussi['libTitrArt'] . '</h3></a>'); ?>
 
                         <div class="bloc-all">
-                            <img src="
-                <?php
-                    echo ($article['urlPhotoArt']);
-                ?>" alt="Image descriptive article">
+                        <img class="image-lune" src="src/images/uploads/<?php echo ($lisezAussi['urlPhotArt']) ?>" alt="Image d'une lune dans l'espace.">
                             <div class="info-all">
-                                <?php echo ('<p>' . $article['dtCreArt'] . '</p>') ?>
+                                <?php echo ('<p>' . $lisezAussi['dtCreArt'] . '</p>') ?>
                             </div>
                         </div>
                     </div>
