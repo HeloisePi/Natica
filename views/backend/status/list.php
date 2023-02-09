35818f44dd1076d111dc4bcb660454f892b36248
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-09 22:33:22 +0100

    correction oublie de div dashboard list

--- views/backend/status/list.php
+++ views/backend/status/list.php
@@ -40,6 +40,6 @@ $statuses = sql_select("STATUT", "*");
             <a href="create.php" class="btn btn-success">Create</a>
         </div>
     </div>
-
+</div>
     <?php
     include '../../../footer.php'; // contains the footer
