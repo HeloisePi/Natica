cc26c2affba060aaedc4cf18fb826fd71dc56a73
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-09 16:53:18 +0100

    Fin du front pour la page des mentions légales

--- rgpd.php
+++ rgpd.php
@@ -26,7 +26,7 @@ include 'header.php';
     <a href="/views/backend/login.php" class="liens-ariane">Mentions légales / RGPD / CGU</a>
 </div>
 
-<div class="container">
+<div class="container toute-la-page">
     <div class="texte-intro">
         <p class="texte-paragraphe">Lors de l’inscription sur le site,
             l’utilisateur s’engage à lire attentivement les Conditions générales d’utilisation et le <b>Règlement général
@@ -46,8 +46,8 @@ include 'header.php';
         <p><b>Mentions légales</b>, d’après la Loi pour la confiance dans l’économie du numérique (LCEN) de 2004.</p>
         <br>
         <p><b>Adresse :&nbsp;</b> 1 Rue Jacques Ellul, 33800 Bordeaux</p>
-        <p><b>Adresse e-mail :</b>&nbsp; Le groupe Natica</p>
-        <p><b>Numéro de téléphone :</b>&nbsp;  05 57 12 20 44</p>
+        <p><b>Adresse e-mail :</b>&nbsp; webmaster-iut@u-bordeaux-montaigne.fr</p>
+        <p><b>Numéro de téléphone :</b>&nbsp; +33 5 57 12 20 44</p>
         <p><b>Hébergement :</b>&nbsp; L'IUT Bordeaux Montaigne</p>
         <br>
         <div class="sous-titre-mentions-legales">Conception</div>
@@ -58,31 +58,31 @@ include 'header.php';
         <br>
         <h1>Conditions Générales d’Utilisation (CGU)</h1>
         <p>Constituant le <b>contrat</b> entre la société IUT Bordeaux Montaigne, l’Utilisateur, 
-            l’accès au site doit être précédé de <b>l’acceptation de ces CGU</b>. 
-            L’accès à cette plateforme signifie l’acceptation des présentes <b>CGU</b>.</p>
+        l’accès au site doit être précédé de <b>l’acceptation de ces CGU</b>. 
+        L’accès à cette plateforme signifie l’acceptation des présentes <b>CGU</b>.</p>
         <p>En utilisant notre site, l’utilisateur d’engage à respecter les conditions générales d’utilisation du site suivante.</p>
         <div class="sous-titre-mentions-legales">Propriété intellectuelle</div>
         <br>
         <p><b>Tout</b> les éléments présent sur le <b>site</b> (photographie, logo, 
-            illustration) sont protégés par la loi en vigueur au titre de la <b>propriété intellectuelle.</b> 
-            En effet toute reproduction ou utilisation est strictement interdite sans accord au préalable de l'éditeur. 
-            Cela vaut pour l’entièreté du site excepté certaines images libres de droits venant de pexel.fr et unsplash.fr. </p>
+        illustration) sont protégés par la loi en vigueur au titre de la <b>propriété intellectuelle.</b> 
+        En effet toute reproduction ou utilisation est strictement interdite sans accord au préalable de l'éditeur. 
+        Cela vaut pour l’entièreté du site excepté certaines images libres de droits venant de pexel.fr et unsplash.fr. </p>
         <p>La <b>reproduction</b> et la <b>copie</b> des contenus par l’Utilisateur requièrent une <b>autorisation</b> préalable du site. Dans ce cas, 
-            toute utilisation à des usages commerciaux ou à des fins publicitaires est <b>proscrite</b>.</p>
+        toute utilisation à des usages commerciaux ou à des fins publicitaires est <b>proscrite</b>.</p>
         <div class="sous-titre-mentions-legales">Champ d’application / contenu utilisateur </div>
         <br>
         <p>Description des services : Nous proposons des services <b>non-marchands</b>, car le site est un blog. 
-            Les services proposés pour les utilisateurs sont la création d’article et l’inscription. </p>
-            <p>Le site est <b>accessible gratuitement</b> depuis n’importe où par tout utilisateur disposant d’un accès à Internet. 
-            <b>Tous</b> les frais nécessaires pour l’accès aux services (matériel informatique, connexion Internet…) sont <b>à la charge de l’utilisateur</b>.</p>
+        Les services proposés pour les utilisateurs sont la création d’article et l’inscription. </p>
+        <p>Le site est <b>accessible gratuitement</b> depuis n’importe où par tout utilisateur disposant d’un accès à Internet. 
+        <b>Tous</b> les frais nécessaires pour l’accès aux services (matériel informatique, connexion Internet…) sont <b>à la charge de l’utilisateur</b>.</p>
         <p>L’accès aux services dédiés aux membres s’effectue à l’aide d’un identifiant et d’un mot de passe.</p>
         <p>Pour des raisons de maintenance ou autres, l’accès au site peut être <b>interrompu</b> ou <b>suspendu</b> par l’éditeur <b>sans préavis ni justification</b>.</p>
         <div class="sous-titre-mentions-legales">Responsabilité</div>
         <br>
         <p>Bien que les informations publiées sur le site soient réputées fiables, le site se réserve la faculté d’une non-garantie de la <b>fiabilité</b> des sources.</p>
         <p>Les informations diffusées sur le blog eXim sont présentées à titre purement <b>informatif</b> et sont <b>sans valeur contractuelle</b>. En dépit des mises à jour régulières, 
-            la <b>responsabilité du site</b> ne peut être engagée en cas de modification des dispositions <b>administratives</b> et <b>juridiques</b> apparaissant <b>après</b> la publication. 
-            Il en est de même pour l’utilisation et l’interprétation des informations communiquées sur la plateforme.</p>
+        la <b>responsabilité du site</b> ne peut être engagée en cas de modification des dispositions <b>administratives</b> et <b>juridiques</b> apparaissant <b>après</b> la publication. 
+        Il en est de même pour l’utilisation et l’interprétation des informations communiquées sur la plateforme.</p>
         <p>Le site décline toute <b>responsabilité</b> concernant les éventuels virus pouvant infecter le matériel informatique de l’Utilisateur après l’utilisation ou l’accès à ce site.</p>
         <p>Le site ne peut être tenu pour <b>responsable</b> en cas de force majeure ou du fait imprévisible et insurmontable d’un tiers.</p>
         <p>La garantie totale de la sécurité et la confidentialité des données n’est pas assurée par le site. Cependant, le site s’engage à mettre en œuvre toutes les méthodes requises pour le faire au mieux.</p>
@@ -93,22 +93,57 @@ include 'header.php';
         <p>Dans ses publications, <b>le membre est tenu de respecter les règles de publications ainsi que les règles de droit en vigueur</b>.
         <p>Le site <b>dispose du droit d’exercer une modération</b> à priori sur les publications et peut refuser leur mise en ligne sans avoir à fournir de justification.</p>
         <p>Le membre garde <b>l’intégralité</b> de ses droits de propriété intellectuelle. Toutefois, toute publication sur le site implique la <b>délégation</b> du droit non exclusif et gratuit à la société éditrice de représenter, 
-            reproduire, modifier, adapter, distribuer et diffuser la publication n’importe où et sur n’importe quel support pour la durée de la propriété intellectuelle. 
-            Cela peut se faire directement ou par l’intermédiaire d’un tiers autorisé. 
-            Cela concerne notamment le droit d’utilisation de la publication sur le web et sur les réseaux de téléphonie mobile.</p>
+        reproduire, modifier, adapter, distribuer et diffuser la publication n’importe où et sur n’importe quel support pour la durée de la propriété intellectuelle. 
+        Cela peut se faire directement ou par l’intermédiaire d’un tiers autorisé. 
+        Cela concerne notamment le droit d’utilisation de la publication sur le web et sur les réseaux de téléphonie mobile.</p>
         <p>À chaque utilisation, l’éditeur s’engage à mentionner le nom du membre à proximité de la publication.</p>
         <p>L’Utilisateur est tenu <b>responsable de tout contenu</b> qu’il met en ligne. 
-            L’Utilisateur <b>s’engage</b> à ne pas publier de contenus susceptibles de porter atteinte aux intérêts de tierces personnes. 
-            Toutes procédures engagées en justice par un tiers lésé à l’encontre du site devront être prises en charge par <b>l’Utilisateur</b>.</p>
-            <p>La <b>suppression</b> ou la <b>modification</b> par le site du contenu de l’Utilisateur peut s’effectuer à <b>tout</b> moment, pour n’importe quelle raison et sans préavis.</p>
-
-
-
+        L’Utilisateur <b>s’engage</b> à ne pas publier de contenus susceptibles de porter atteinte aux intérêts de tierces personnes. 
+        Toutes procédures engagées en justice par un tiers lésé à l’encontre du site devront être prises en charge par <b>l’Utilisateur</b>.</p>
+        <p>La <b>suppression</b> ou la <b>modification</b> par le site du contenu de l’Utilisateur peut s’effectuer à <b>tout</b> moment, pour n’importe quelle raison et sans préavis.</p>
+        <div class="sous-titre-mentions-legales">Règlement Général sur la Protection des Données (RGPD)</div>
+        <br>
+        <p>Conformément à la <b>loi n°78-17</b> du 6 janvier relative à l’informatique, aux fichiers et aux libertés, 
+        la collecte et le traitement d’informations personnelles s’effectuent dans le <b>respect de la vie privée</b>.</p>
+        <p>Selon le Règlement général sur la protection des données (<b>RGPD</b>) du 25 mai 2018 et conformément à la <b>loi n°78-17</b> du 6 janvier relative à l’informatique, 
+        aux fichiers et aux libertés, la collecte et le traitement d’informations personnelles s’effectuent dans le respect de la vie privée, 
+        lors de l’inscription sur le site, vous nous <b>autorisez</b> à collecter les données suivantes :</p>
+        <br>
+        <p><b>Adresse e-mail</p>
+        <p>Nom</p>
+        <p>Prénom</b></p>
+        <br>
+        <p>Les informations recueillies sur ce formulaire sont enregistrées dans un fichier informatisé par 
+        <b>IUT Bordeaux Montaigne</b>, 1 rue Jacques Ellul 33080 Bordeaux Cedex France, [+33 5 57 12 20 44](tel:+33557122044), 
+        [webmaster-iut@u-bordeaux-montaigne.fr](mailto:webmaster-iut@u-bordeaux-montaigne.fr)
+        pour le bon <b>fonctionnement</b> de votre compte sur le site et seront communiquées au seul destinataire suivant : Marlène Dulaurans.</p>
+        <p>Vous pouvez accéder aux données vous concernant, les rectifier, 
+        demander leur effacement ou exercer votre droit à la limitation du traitement de vos données.</p>
+        <p>Suivant la <b>loi Informatique et Libertés</b> en date du 6 janvier 1978, <b>articles 39 et 40</b>, 
+        les données sont <b>conservées</b> à compter de l’achèvement de la base et jusqu’à l’issue d’une période de 15 ans à partir du 1er janvier de l’année qui suit la date d’achèvement.</p>
+        <p>D’après le droit à la <b>portabilité des données</b>, vous pourrez <b>récupérer</b> les données qu’elle a fournies sous une forme aisément réutilisable.</p>
+        <p>Les <b>mineurs</b> (moins de 16 ans) devront avoir un <b>consentement recueilli auprès de l’autorité parentale</b>.</p>
+        <br>
+        <p>Pour exercer ces droits ou pour toute question sur le traitement de vos données dans ce dispositif, 
+        vous pouvez contacter notre délégué à la protection des données ou le service chargé de l’exercice de ces droits : </p>
+        <br>
+        <p>IUT Bordeaux Montaigne</p>
+        <p>1 rue Jacques Ellul 33080 Bordeaux Cedex France</p>
+        <p>+33 5 57 12 20 44</p>
+        <p>webmaster-iut@u-bordeaux-montaigne.fr</p>
+        <br>
+        <br>
+        <br>
+        <p>Si vous estimez, après nous avoir contactés, que vos droits « <b>Informatique et Libertés</b> » ne sont pas respectés, 
+        vous pouvez adresser une réclamation à la <b>CNIL</b>. </p>
+        <br>
     </div>
 
 </div>
 
-
+<div class="logo-fin-mentions-legales">
+    <img src="/images/eXim-logo.svg" alt="Logo eXim">
+</div>
 
 <?php
 include 'footer.php';
