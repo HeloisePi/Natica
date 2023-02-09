084184fec3f2f190d9a7f2a69c4df28b364d1d76
TristanREB <tristan.rebeyrol@mmibordeaux.com>
2023-02-09 23:54:49 +0100

    ajout image articles

--- articles.php
+++ articles.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,55 +10,53 @@
     <link rel="reset.css" href="/src/css/reset.css">
     <link rel="stylesheet" href="/src/css/articles.css">
     <link rel="stylesheet" href="https://use.typekit.net/ujw8boj.css">
-    <link rel="stylesheet" href="/assert/Fraunces-VariableFont_SOFT,WONK,opsz,wght.ttf"><link rel="stylesheet" href="/assert/Montserrat-Italic-VariableFont_wght.ttf">
+    <link rel="stylesheet" href="/assert/Fraunces-VariableFont_SOFT,WONK,opsz,wght.ttf">
+    <link rel="stylesheet" href="/assert/Montserrat-Italic-VariableFont_wght.ttf">
 </head>
 
 <body>
-<?php
-include 'header.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
+    <?php
+    include 'header.php';
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 
-$articles = sql_select('article', '*', "", "numArt DESC");
+    $articles = sql_select('article', '*', "", "numArt DESC")[0];
 
-?>
+    ?>
 
-<div class="fil-ariane3">
-    <a href="/index.php" class="liens-ariane">Accueil</a>
-    <p class="separation"> > </p>
-    <a href="/articles.php" class="liens-ariane">Articles</a>
-</div>
+    <div class="fil-ariane3">
+        <a href="/index.php" class="liens-ariane">Accueil</a>
+        <p class="separation"> > </p>
+        <a href="/articles.php" class="liens-ariane">Articles</a>
+    </div>
 
-<div class="all-articles">
-    <form class="d-flex" role="search">
+    <div class="all-articles">
+        <form class="d-flex" role="search">
             <div class="col-md-5">
                 <input class="form-control m-2 taille-saisie" type="search" placeholder="Rechercher..." aria-label="Rechercher...">
                 <button class="search" type="submit">Rechercher</button>
             </div>
-        </div>
+    </div>
     </form>
     <h1 class="experiences-immersives">expériences immersives de 2023-2024</h1>
     <div class="rect-all">
 
-        <?php foreach($articles as $article){?>
-            <div class= "titres-all">
-                <?php echo('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>');?>
-                
-            <div class="bloc-all">
-                <img src="
-                <?php
-                echo ($article['urlPhotoArt']);
-                ?>" alt="Image descriptive article">
-                <div class="info-all">
-                    <?php echo('<p>' . $article['dtCreArt']. '</p>') ?>
+        <?php foreach ($articles as $article) { ?>
+            <div class="titres-all">
+                <?php echo ('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>'); ?>
+
+                <div class="bloc-all">
+                    <img src="src/images/uploads/<?php echo ($articles['urlPhotArt']) ?>" alt="Image descriptive article">
+                    <div class="info-all">
+                        <?php echo ('<p>' . $article['dtCreArt'] . '</p>') ?>
+                    </div>
                 </div>
             </div>
-            </div>
-            <?php }?>
+        <?php } ?>
     </div>
-                
-        
-            <!-- <div class= "titres-all">
+
+
+    <!-- <div class= "titres-all">
                 <h3>Evènement 2</h3>
                 <div class="bloc-all">
                     <div class="info-all">
@@ -72,8 +71,10 @@ $articles = sql_select('article', '*', "", "numArt DESC");
                 </div>
             </div>    
         </div> -->
-</div>
+    </div>
 
-<?php include 'footer.php'; // contains the footer ?>
+    <?php include 'footer.php'; // contains the footer 
+    ?>
 </body>
+
 </html>
