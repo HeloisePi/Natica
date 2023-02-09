3848a945fd3c5021e72847b467b05041e0cdaeb8
Hugo-mensah <116188727+Hugo-mensah@users.noreply.github.com>
2023-02-09 14:40:36 +0100

    debut page mentions legals

---
+++ rgpd.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Événement - eXim</title>
+    <link rel="stylesheet" href="/src/css/connection.css">
+    <link rel="stylesheet" href="/src/css/evenement.css">
+    <link rel="stylesheet" href="https://use.typekit.net/ujw8boj.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+include 'header.php';
+?>
+
+
+
+
+
+<?php
+include 'footer.php';
+?>
