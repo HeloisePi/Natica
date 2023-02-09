1c837eb1e6f13a94f3bb9e4b2755dfe6393a559d
EloLsn <elodie.laisne@mmibordeaux.com>
2023-02-09 17:42:09 +0100

    ariane-create-articles

--- views/backend/articles/create.php
+++ views/backend/articles/create.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Écrire un article - eXim</title>
     <link rel="stylesheet" href="/src/css/main.css">
-    <link rel="stylesheet" href="/src/css/connection.css">
+
 </head>
 <body>
     
