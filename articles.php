302578762fa39836ede4908cef2b5077f4e0301e
Safirl <loic.leforestier@mmibordeaux.com>
2023-02-10 11:40:06 +0100

    bug articles svp arrêtez de toucher à cette page

--- articles.php
+++ articles.php
@@ -46,10 +46,10 @@
                 <?php echo ('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>'); ?>
 
                 <div class="bloc-all">
-                    <img class="image-lune" src="<?php echo ($article['urlPhotArt']) ?>" alt="Image d'une lune dans l'espace.">
                     <div class="info-all">
                         <?php echo ('<p>' . $article['dtCreArt'] . '</p>') ?>
                     </div>
+                    <img class="image-lune" src="<?php echo ($article['urlPhotArt']) ?>" alt="Image d'une lune dans l'espace.">
                 </div>
             </div>
         <?php
