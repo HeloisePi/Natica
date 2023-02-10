38854c303ae7edf022ab01c0c4cdc592b159e68d
TristanREB <tristan.rebeyrol@mmibordeaux.com>
2023-02-10 11:00:23 +0100

    oui

--- articles.php
+++ articles.php
@@ -38,22 +38,23 @@
     </div>
     </form>
     <h1 class="experiences-immersives">expériences immersives de 2023-2024</h1>
-    <div class="rect-all">
+    <?php foreach ($articles as $article) { ?>
 
-        <?php foreach($articles as $article) { ?>
-        <div class= "titres-all">
-            <?php echo('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>');?>
-                
-            <div class="bloc-all">
-                <img class="image-lune" src="/images/lune.svg" alt="Image d'une lune dans l'espace.">
-                <div class="info-all">
-                    <?php echo('<p>' . $article['dtCreArt']. '</p>') ?>
-            <?php 
-        }
-        ?>
+        <div class="rect-all">
+
+            <div class="titres-all">
+                <?php echo ('<a href="/evenement.php?numArt=' . $article['numArt'] . ' "><h3>' . $article['libTitrArt'] . '</h3></a>'); ?>
+
+                <div class="bloc-all">
+                    <img class="image-lune" src="<?php echo ($article['urlPhotArt']) ?>" alt="Image d'une lune dans l'espace.">
+                    <div class="info-all">
+                        <?php echo ('<p>' . $article['dtCreArt'] . '</p>') ?>
+                    <?php
+                }
+                    ?>
+                    </div>
                 </div>
             </div>
-        </div>
 
             <!-- <div class= "titres-all">
                 <h3>Evènement 2</h3>
@@ -70,8 +71,8 @@
                 </div>
             </div>    
         </div> -->
-    </div>
-    <?php require_once 'footer.php';?>
+        </div>
+        <?php require_once 'footer.php'; ?>
 </body>
 
 </html>
